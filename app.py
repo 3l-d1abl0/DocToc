d4fdc874b233be86dd904779bfebe1f4114b1137
Sameer <sameer.barha12@gmail.com>
2024-06-24 01:03:50 +0530

    read pfd

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,3 +21,10 @@ if __name__ == "__main__":
 
     #Upload a PDF file
     pdf = st.file_uploader('Upload you pdf file', type='pdf')
+
+    if pdf:
+        pdf_reader = PdfReader(pdf)
+        text=""
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+        st.write(text)
